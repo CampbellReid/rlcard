3ca02f803d347ab0e6907140bc86e939f44931b6
Campbell Reid <campbell@campbellreid.com>
2024-11-07 14:01:58 +1000

    Create a placeholder file for a script for a human to play against 3 other agents.

---
+++ examples/human/five_hundred_human.py
@@ -0,0 +1,7 @@
+'''
+    File name: five_hundred_human.py
+    Author: Campbell Reid
+    Date created: 2024-11-07
+    
+    An example of a human playing in a game of Five Hundred against 3 other agents.
+'''
