313fff8a93404ab44e7a7e1e8dbd6f29b63bd260
Campbell Reid <campbell@campbellreid.com>
2024-11-27 11:59:08 +1000

    Correct error in copying code across to five_hundred_human.py and five_hundred_human_agent.py. Copy the code from blackjack_human.py to five_hundred_human.py.

--- examples/human/five_hundred_human.py
+++ examples/human/five_hundred_human.py
@@ -6,73 +6,68 @@
     An example of a human playing in a game of Five Hundred against 3 other agents.
 '''
 
+import rlcard
+from rlcard.agents import RandomAgent as RandomAgent
+from rlcard.agents import BlackjackHumanAgent as HumanAgent
 from rlcard.utils.utils import print_card
 
+# Make environment
+num_players = 2
+env = rlcard.make(
+    'blackjack',
+    config={
+        'game_num_players': num_players,
+    },
+)
+human_agent = HumanAgent(env.num_actions)
+random_agent = RandomAgent(env.num_actions)
+env.set_agents([
+    human_agent,
+    random_agent,
+])
 
-class HumanAgent(object):
-    ''' A human agent for Five Hundred. It can be used to play alone to understand how the five hundred code runs
-    '''
+print(">> Blackjack human agent")
 
-    def __init__(self, num_actions):
-        ''' Initilize the human agent
+while (True):
+    print(">> Start a new game")
 
-        Args:
-            num_actions (int): the size of the output action space
-        '''
-        self.use_raw = True
-        self.num_actions = num_actions
+    trajectories, payoffs = env.run(is_training=False)
+    # If the human does not take the final action, we need to
+    # print other players action
 
-    @staticmethod
-    def step(state):
-        ''' Human agent will display the state and make decisions through interfaces
+    if len(trajectories[0]) != 0:
+        final_state = []
+        action_record = []
+        state = []
+        _action_list = []
 
-        Args:
-            state (dict): A dictionary that represents the current state
+        for i in range(num_players):
+            final_state.append(trajectories[i][-1])
+            state.append(final_state[i]['raw_obs'])
 
-        Returns:
-            action (int): The action decided by human
-        '''
-        _print_state(state['raw_obs'], state['raw_legal_actions'], state['action_record'])
-        action = int(input('>> You choose action (integer): '))
-        while action < 0 or action >= len(state['legal_actions']):
-            print('Action illegal...')
-            action = int(input('>> Re-choose action (integer): '))
-        return state['raw_legal_actions'][action]
+        action_record.append(final_state[i]['action_record'])
+        for i in range(1, len(action_record) + 1):
+            _action_list.insert(0, action_record[-i])
 
-    def eval_step(self, state):
-        ''' Predict the action given the current state for evaluation. The same to step here.
+        for pair in _action_list[0]:
+            print('>> Player', pair[0], 'chooses', pair[1])
 
-        Args:
-            state (numpy.array): an numpy array that represents the current state
+    # Let's take a look at what the agent card is
+    print('===============   Dealer hand   ===============')
+    print_card(state[0]['state'][1])
 
-        Returns:
-            action (int): the action predicted (randomly chosen) by the random agent
-        '''
-        return self.step(state), {}
+    for i in range(num_players):
+        print('===============   Player {} Hand   ==============='.format(i))
+        print_card(state[i]['state'][0])
 
-def _print_state(state, raw_legal_actions, action_record):
-    ''' Print out the state
+    print('===============     Result     ===============')
+    for i in range(num_players):
+        if payoffs[i] == 1:
+            print('Player {} win {} chip!'.format(i, payoffs[i]))
+        elif payoffs[i] == 0:
+            print('Player {} is tie'.format(i))
+        else:
+            print('Player {} lose {} chip!'.format(i, -payoffs[i]))
+        print('')
 
-    Args:
-        state (dict): A dictionary of the raw state
-        action_record (list): A list of the each player's historical actions
-    '''
-    _action_list = []
-    for i in range(1, len(action_record)+1):
-        _action_list.insert(0, action_record[-i])
-    for pair in _action_list:
-        print('>> Player', pair[0], 'chooses', pair[1])
-
-    # TODO: Implement equivalent state printout for Five Hundred. Below is the Blackjack equivalent
-    # print('\n=============   Dealer Hand   ===============')
-    # print_card(state['dealer hand'])
-
-    # num_players = len(state) - 3
-
-    # for i in range(num_players):
-    #     print('===============   Player {} Hand   ==============='.format(i))
-    #     print_card(state['player' + str(i) + ' hand'])
-
-    print('\n=========== Actions You Can Choose ===========')
-    print(', '.join([str(index) + ': ' + action for index, action in enumerate(raw_legal_actions)]))
-    print('')
+    input("Press any key to continue...")
